1283d3e8125325e8fd341a821869bc601c3d7321
zzolikoff1027 <78103329+zzolikoff1027@users.noreply.github.com>
2022-10-21 12:28:37 -0700

    Add files via upload

---
+++ Assets/Scripts/VehicleRequirements.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleRequirements
+{
+    public int NumberOfWheels;
+    public bool Engine;
+    public int Passengers;
+}
